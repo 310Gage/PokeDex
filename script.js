c09c1937d0c36f0ae7547948b91971236e472f20
Gage Hall <Gageh310@gmail.com>
2022-12-04 23:35:52 -0500

    PokeDex Website Using API's

---
+++ script.js
@@ -0,0 +1,23 @@
+// (function titleChanger(text){
+//     document.title = text;
+//     setTimeout(function () {
+//         titleChanger(text.substr(2) + text.substr(0, 2));
+//     }, 200)
+// })(" Gage's Portfolio ")
+
+
+
+
+
+
+do
+{
+
+    document.title = `Gage's portfolio`;
+    setTimeout(200);
+    document.title = `Gage's portfoli`;
+    setTimeout(200);
+    document.title = `Gage's portfol`;
+    setTimeout(200);
+
+} while (true)
